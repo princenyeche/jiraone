b3a181a6f7ae15b13e844d2e53cff23076627807
Prince <hakwumerunyeche@gmail.com>
2021-01-04 15:53:41 +0800

    PyPI setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jiraone",
+    version="0.1.0",
+    author="Prince Nyeche",
+    author_email="support@elfapp.website",
+    description="A REST API Implementation to Jira Cloud APIs for creating reports",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/princenyeche/atlassian-cloud-api",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
