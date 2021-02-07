15e9cad0475d198a7a32a2ef1b941b1e09d11d2c
Prince <hakwumerunyeche@gmail.com>
2021-02-07 12:54:50 +0800

    updated to v0.2.0

--- jiraone/__init__.py
+++ jiraone/__init__.py
@@ -20,9 +20,9 @@ You can dynamically generate a report of
 12. Total no of attachments on the project and on the instance
 and many more depending on what you can come up with.
 """
-from jiraone.access import LOGIN, endpoint, echo
+from jiraone.access import LOGIN, endpoint, echo, For
 from jiraone.jira_logs import add_log, WORK_PATH
-from jiraone.reporting import PROJECT, USER, file_writer, file_reader, path_builder
+from jiraone.reporting import PROJECT, USER, file_writer, file_reader, path_builder, replacement_placeholder
 
 __all__ = ["LOGIN",
            "endpoint",
@@ -33,4 +33,6 @@ __all__ = ["LOGIN",
            "USER",
            "file_writer",
            "file_reader",
-           "path_builder"]
+           "path_builder",
+           "replacement_placeholder",
+           "For"]
