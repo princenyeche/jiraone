eac26bd91bd79a6f9107552bea59d08c31155419
Prince <hakwumerunyeche@gmail.com>
2021-08-31 08:45:52 +0200

    v0.4.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="jiraone",
-    version="0.4.1",
+    version="0.4.2",
     author="Prince Nyeche",
     author_email="support@elfapp.website",
     description="A REST API Implementation to Jira Cloud APIs for creating reports and "
