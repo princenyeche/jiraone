6b59d9d7fde6e43c1572d279ef5d7b4ae01656b4
Prince <hakwumerunyeche@gmail.com>
2021-10-11 23:29:18 +0200

    update to v0.4.6

--- jiraone/module.py
+++ jiraone/module.py
@@ -3,9 +3,11 @@
 """
 Includes any other functions or global variables
 """
+import os
 import re
 import json
-from typing import Union, Any, Optional, List, Callable
+from typing import Union, Any, Optional, List, Callable, Dict, NoReturn
+import requests
 from jiraone.exceptions import JiraOneErrors
 from collections import namedtuple, deque
 from datetime import datetime as dt
@@ -85,11 +87,11 @@ def field_update(field, key_or_id: Union[str, int], name: Optional[str] = None, 
 
 def time_in_status(
         # a variable to call the `PROJECT` alias of `jiraone.report.PROJECT`
-        var: Optional[Any],
+        var: Any,
         # issue key or id of an issue, or a list of issue key or id
         key_or_id: Union[str, int, List[Union[str, int]], dict],
         # a file reader function
-        reader: Optional[Callable] = None,
+        reader: Callable = None,
         # A file name used to store the output file
         report_file: Optional[str] = "time_status.csv",
         # a folder which can used to store the file
@@ -316,6 +318,206 @@ def pretty_format(date: Any, pprint: bool) -> str:
                 d = get_numbers.findall(make_date)
                 return f"{d[0]}h {d[1]}m {d[2]}s"
     return make_date
+
+
+def bulk_change_email(data: str, token: str) -> None:
+    """Bulk change managed user's email address if they do not exist.
+
+    :param data A string of the file name
+
+    :param token A string of the API token to authenticate the request
+
+    Additionally, the output of failed attempts it written back to your data file.
+
+    :return: None
+    """
+    from jiraone import file_reader, file_writer, manage as org
+    collect = deque()
+    # Provide a CSV file data source
+    read = file_reader(file_name=data, skip=True)
+    data_check = deepcopy(read)
+    headers = ["account_id", "current_email", "name", "target_email"]
+    file_writer(file_name=data, mode="w+", mark="single", data=headers)
+    if len(data_check[0]) != 4:
+        raise JiraOneErrors("value", f"The expected data column should be 4 columns got {data_check[0]} instead")
+    items = namedtuple("items", ["account_id", "current_email", "name", "target_email"])
+    org.add_token(token)
+    for _ in read:
+        user = items._make(_)
+        payload = {
+            "email": user.target_email
+        }
+        response = org.manage_profile(account_id=user.account_id, method="put", json=payload)
+        if response.status_code < 300:
+            print(f"Changed current email: {user.current_email} to target email: {user.target_email}")
+        else:
+            print(f"Not able to change current email: {user.current_email} to target email: {user.target_email}")
+            collect.append(_)
+
+    # If any of the email address isn't changed, write the output to the same file.
+    file_writer(file_name=data, mode="a+", mark="many", data=collect)
+    print("Change process completed".upper())
+    collect.clear()
+
+
+def bulk_change_swap_email(data: str, token: str, **kwargs: Any) -> NoReturn:
+    """The function helps to swap an email address to another.
+
+    If the target email exist as an Atlassian account email, this will help to swap
+    that email address to the desired target email.
+
+    :param data A string of the file name
+
+    :param token An API token to authenticate the API.
+
+    :return: None
+    """
+    from jiraone import file_reader, file_writer, manage as org
+    collect = deque()
+    org.add_token(token)
+    # Provide a CSV file data source
+    read = file_reader(file_name=data, skip=True)
+    data_check = deepcopy(read)
+    user_collection = deque()
+    copy_name = data.split('.')[0] + "_cp.csv"
+    headers = ["account_id", "current_email", "name", "target_email", "reason"]
+    headers_2 = ["account_id", "email"]
+    file_writer(file_name=data, mode="w+", mark="single", data=headers)
+    file_writer(file_name=copy_name, mode="w+", mark="single", data=headers_2)
+    dummy = kwargs['dummy'] if 'dummy' in kwargs else ""
+    if len(data_check[0]) != 4:
+        raise JiraOneErrors("value", f"The expected data column should be 4 columns got {len(data_check[0])} instead")
+    user_items = namedtuple("user_items", ["account_id", "email"])
+    items = namedtuple("items", ["account_id", "current_email", "name", "target_email"])
+    # If the user is not on premium/enterprise plan they need to supply their own user_list for find_id()
+    # If not the swap won't work correctly.
+    users = file_reader(file_name=kwargs['users'], skip=True) if 'users' in kwargs else None
+    data_response = {'count': 0, 'email': None}
+
+    def get_all_users(source) -> NoReturn:
+        """Store all user list from organization, so we can search them by email.
+        :param source A JSON response payload
+
+        :return: None
+        """
+        print("Checking organization users...")
+        while True:
+            next_item_data = source['links']['next'] if 'links' in source \
+                                                        and len(source['links']) > 1 else {}
+            for item in source['data']:
+                user_data = {
+                    "account_id": item['account_id'],
+                    "email": item['email']
+                }
+                user_collection.append(list(user_data.values()))
+            # If our next item is an empty dict, we want to stop the loop.
+            if isinstance(next_item_data, dict):
+                break
+            source = requests.get(next_item_data, headers=org.AUTH).json()
+
+    source_data = org.get_organization(filter_by="users").json()
+    get_all_users(source_data)
+    file_writer(file_name=copy_name, mark="many", mode="a+", data=user_collection)
+    user_collection.clear()
+    get_file = file_reader(file_name=copy_name, skip=True)
+
+    def find_id(email: str) -> Any:
+        """Return a string if email is found else None.
+
+        :param email: A string
+
+        :return: str if email is found else None.
+        """
+        data_parse = get_file if 'users' not in kwargs else users
+        for z in data_parse:
+            obtain = user_items._make(z)
+            if email in obtain._asdict().values():
+                return obtain.account_id
+
+    def dummy_swap(emails: Any, count: int) -> Dict:
+        """Returns a dictionary
+
+            :param emails: A namedtuple to string of an email address
+
+            :param count: An integer count, basically trying to see if we can keep track of the changes.
+
+            :return: dict
+            """
+        check_email = org.manage_profile(emails.account_id).json()
+        if dummy == check_email.get("account").get("email"):
+            access = {
+                "email": emails.target_email
+            }
+            result = org.manage_profile(emails.account_id, json=access, method="put")
+            if result.status_code < 300:
+                count += 1
+                return {"result": result, "count": count, "email": emails.target_email}
+        return {"result": None, "count": count, "email": None}
+
+    def target_swap(emails: Any, count: int) -> Dict:
+        """Returns a dictionary
+
+            :param emails: A namedtuple to string
+            :param count: An integer count, basically trying to see if we can keep track of the changes.
+
+            :return: dict
+            """
+        access = {
+            "email": emails.current_email
+        }
+        get_id = find_id(emails.target_email)
+        if get_id is not None:
+            result = org.manage_profile(get_id, json=access, method="put")
+            if result.status_code < 300:
+                count += 1
+                return {"result": result, "count": count, "email": emails.current_email}
+        return {"result": None, "count": count, "email": None}
+
+    swap_completion = 0
+    for _ in read:
+        user = items._make(_)
+        current = user.current_email.split("@")[1]
+        dummy = f"dummy0000008@" + current if 'dummy' not in kwargs else kwargs['dummy']
+        payload = {
+            "email": dummy
+        }
+
+        response = org.manage_profile(user.account_id, method="put", json=payload)
+        if response.status_code < 300:
+            print("Success:", f"Status: {response.status_code}", sep="---")
+            print(f"Changing current email: {user.current_email} to dummy email: {dummy}")
+            data_response['count'] += 1
+            if data_response['count'] > 0:
+                data_response = target_swap(user, data_response['count'])
+                if data_response['email'] is not None:
+                    print(f"Changing target email: {user.target_email} to current email: {data_response['email']}")
+                    data_response = dummy_swap(user, data_response['count'])
+                    if data_response['email'] is not None:
+                        print(f"Changing dummy email: {dummy} to target email: {data_response['email']}")
+                        swap_completion += 1
+                        data_response['count'] -= 2
+                    else:
+                        print(
+                            f"Unable to change dummy email: {dummy} to target email: {data_response['email']}")
+                else:
+                    print(
+                        f"Unable to change target email: {user.target_email} to current email: {data_response['email']}")
+                    error_data = [user.account_id, user.current_email, user.name, user.target_email,
+                                  data_response['result']]
+                    collect.append(error_data)
+        else:
+            print("Error:", f"status: {response.status_code}", f"Reason: {response.reason}", sep="---")
+            print(f"Unable to change current email: {user.current_email} to dummy email: {dummy}")
+            error_data = [user.account_id, user.current_email, user.name, user.target_email, response.reason]
+            collect.append(error_data)
+        print("*" * 100)
+
+    # For any email that is not changed, write it back to the same file
+    file_writer(file_name=data, mode="a+", mark="many", data=collect)
+    print("Process complete, made swap attempt of {} email(s), complete cycle swap of {} email(s)."
+          .format(data_response['count'], swap_completion))
+    os.remove(copy_name)
+    collect.clear()
 
 
 permissions = Permissions()
