b12dd7f2eb3b7323a6cf73d3f3577d19e2aa0177
Prince <hakwumerunyeche@gmail.com>
2022-01-06 19:42:53 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('..'))
+from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
 
@@ -47,8 +48,10 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = ".rst"
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
 
 # The master toctree document.
 master_doc = 'index'
@@ -59,6 +62,12 @@ master_doc = 'index'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 # language = None
+
+extensions = ['autoapi.extension']
+
+# Document Python Code
+autoapi_type = 'python'
+autoapi_dir = '../src'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
