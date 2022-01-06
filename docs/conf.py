af9cca5200383b7faebe0821f48dd62fd9377c30
Prince <hakwumerunyeche@gmail.com>
2022-01-06 19:52:27 +0100

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,6 @@
 # import os
 # import sys
 # sys.path.insert(0, os.path.abspath('..'))
-from recommonmark.parser import CommonMarkParser
 
 # -- Project information -----------------------------------------------------
 
@@ -47,10 +46,7 @@ templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
-#
-source_parsers = {
-    '.md': CommonMarkParser,
-}
+
 source_suffix = ['.rst', '.md']
 
 # The master toctree document.
