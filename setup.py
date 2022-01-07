3c372f327da1a9e687494fd6a27c56432df3fa52
Prince <hakwumerunyeche@gmail.com>
2022-01-07 13:24:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
 import setuptools
 
-setuptools.setup()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jiraone",
+    version="0.5.0",
+    author="Prince Nyeche",
+    author_email="support@elfapp.website",
+    description="A REST API Implementation to Jira Cloud APIs for creating reports and "
+                "for performing other Jira queries.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/princenyeche/jiraone",
+    packages=setuptools.find_packages(),
+    install_requires=['requests'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
