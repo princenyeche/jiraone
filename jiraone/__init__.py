7a94f272e161353a7d536e83cf13e54b3af5175a
Prince <hakwumerunyeche@gmail.com>
2022-09-04 09:18:13 +0200

    Upcoming update to v0.7.0
    * Minor changes to the reporting API

--- jiraone/__init__.py
+++ jiraone/__init__.py
@@ -23,7 +23,7 @@ and many more depending on what you can come up with.
 from jiraone.access import LOGIN, endpoint, echo, For, field
 from jiraone.jira_logs import add_log, WORK_PATH
 from jiraone.reporting import PROJECT, USER, file_writer, file_reader, path_builder, \
-    replacement_placeholder, comment, delete_attachments
+    replacement_placeholder, comment, delete_attachments, issue_export
 from jiraone.management import manage
 
 __author__ = "Prince Nyeche"
@@ -43,4 +43,5 @@ __all__ = ["LOGIN",
            "field",
            "comment",
            "manage",
-           "delete_attachments"]
+           "delete_attachments",
+           "issue_export"]
