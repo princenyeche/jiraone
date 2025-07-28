e2e22207f600ebb20867aa324f7220e9406b260e
Prince <hakwumerunyeche@gmail.com>
2025-07-29 00:44:03 +0200

    Update to init file

--- src/jiraone/__init__.py
+++ src/jiraone/__init__.py
@@ -37,7 +37,7 @@ from jiraone.reporting import (
 from jiraone.management import manage
 
 __author__ = "Prince Nyeche"
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __all__ = [
     "LOGIN",
     "endpoint",
